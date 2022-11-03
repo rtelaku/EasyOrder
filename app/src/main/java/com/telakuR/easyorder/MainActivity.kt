d535b9d14461ca06411dea1798d71a3a66736563
Rigeita <rigeita@Rigeitas-MacBook-Pro.local>
2022-11-03 19:16:08 +0100

    Set up splash screen and app icon

--- app/src/main/java/com/telakuR/easyorder/MainActivity.kt
+++ app/src/main/java/com/telakuR/easyorder/MainActivity.kt
@@ -10,15 +10,30 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.telakuR.easyorder.ui.theme.EasyOrderTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             EasyOrderTheme {
-
+                Text(text = "Hi")
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    EasyOrderTheme {
+        Greeting("Android")
     }
 }
